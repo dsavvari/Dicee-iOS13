c844f9b5223ebfcf0477a59184d92c21685269c4
Dimitris Savvaris <dimitris@computer.org>
2025-02-04 14:18:35 +0000

    Dicee launch screen layout updates

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix");
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceFour");
+
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceRollValues = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix")
+        ];
+        
+        diceImageViewOne.image = diceRollValues.randomElement()
+        diceImageViewTwo.image = diceRollValues.randomElement()
 
+    }
 }
 
